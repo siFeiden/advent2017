61f4dcacfade1571b72b38a8f4ab07491c28a2a4
Simon Feiden <simon.feiden@rwth-aachen.de>
2017-11-30 13:36:43 +0100

    Refactor Door and io to have a concise api

--- advent/door.py
+++ advent/door.py
@@ -1,9 +1,15 @@
-from advent.doorio import DoorSuccessReader, DoorTemplateModuleLoader, CopyDoorFile
+from advent.doorio import DoorType, DoorFile, DoorSuccessReader
+
+from os import path
+from advent import doorio
 
 
 class DoorInfoSplit(object):
   def __init__(self, text):
-    self.text = text
+    if text:
+      self.text = text.strip()
+    else:
+      self.text = '\n'
 
   def split_title_description(self):
     lines = self.text.splitlines()
@@ -14,16 +20,35 @@ class DoorInfoSplit(object):
 
 
 class Door(object):
-  def __init__(self, day, module):
+  def __init__(self, day):
     self.day = day
-    self.title, self.description = DoorInfoSplit(module.__doc__.strip()).split_title_description()
     self.done = DoorSuccessReader(day).read()
 
-  def copy_door_file(self):
-    CopyDoorFile(self.day).copy()
+    self.template = DoorFile(day, DoorType.Template)
+    self.test = DoorFile(day, DoorType.Test)
+    self.solution = DoorFile(day, DoorType.Solution)
+
+    if self.template.exists():
+      template_module = self.template.module()
+      t, d = DoorInfoSplit(template_module.__doc__).split_title_description()
+      self.title = t
+      self.description = d
+    else:
+      self.title = ''
+      self.description = ''
+
+  def is_loaded(self):
+    return self.template.exists() and self.test.exists()
+
+  def load(self):
+    self.template.load_from(template_url)
+    self.test.load_from(test_url)
+
+  def show(self):
+    self.template.copy_to(self.solution)
+    return self.solution
 
   @staticmethod
   def for_day(day):
-    mod = DoorTemplateModuleLoader(day).load()
-    return Door(day, mod)
+    return Door(day)
 
--- advent/doorio.py
+++ advent/doorio.py
@@ -1,5 +1,6 @@
 from os import path
 
+import enum
 import importlib.util
 import json
 import shutil
@@ -9,25 +10,10 @@ TEMPLATES_BASE_PATH = 'doors'
 BASE_PATH = 'tuerchen'
 
 
-class CopyNoOverwriteFile(object):
-  def __init__(self, src, dest):
-    self.src = src
-    self.dest = dest
-
-  def copy(self):
-    if not path.exists(self.dest):
-      shutil.copyfile(self.src, self.dest)
-
-
-class CopyDoorFile(CopyNoOverwriteFile):
-  def __init__(self, day):
-    file_name = 'tuerchen{}.py'.format(day)
-    door_file = path.join(BASE_PATH, file_name)
-
-    template_name = 'door{}.py'.format(day)
-    door_template = path.join(TEMPLATES_BASE_PATH, template_name)
-    super().__init__(door_template, door_file)
-
+class DoorType(enum.Enum):
+    Template = 'doors/door{}.py'
+    Test = 'doors/test{}.py'
+    Solution = 'tuerchen/tuerchen{}.py'
 
 class DoorSuccessReader(object):
   def __init__(self, day):
@@ -56,37 +42,37 @@ class DoorSuccessUpdate(object):
       json.dump(stat, f)
 
 
-class DoorModuleLoader(object):
-  MODULE_NAME_PATTERN = 'door{}'
-  SUBDIR = TEMPLATES_BASE_PATH
-
-  def __init__(self, day):
+class DoorFile(object):
+  def __init__(self, day, door_type):
     self.day = day
+    self.door_type = door_type
+    self.file_path = door_type.value.format(day)
+    self._module = None
 
-  def load(self):
+  def exists(self):
+    return path.exists(self.file_path)
+
+  def load_from(self, url):
+    return HttpDownloadFile(url, self.file_path).get()
+
+  def copy_to(self, dest, overwrite=False):
+    if overwrite or (not dest.exists()):
+      shutil.copyfile(self.file_path, dest.file_path)
+    
+  def module(self):
+    if self._module:
+      return self._module
+
     try:
-      module_name = self.MODULE_NAME_PATTERN.format(self.day)
-      file_name = path.join(self.SUBDIR, module_name + '.py')
+      module_name = self.door_type.name.lower() + str(self.day)
 
-      spec = importlib.util.spec_from_file_location(module_name, file_name)
+      spec = importlib.util.spec_from_file_location(module_name, self.file_path)
       mod = importlib.util.module_from_spec(spec)
       spec.loader.exec_module(mod)
+
+      self._module = mod
       return mod
-    except Exception as e:
+    except Exception:
+      # TODO: notify in case of parsing errors
       return None
-
-
-class DoorTemplateModuleLoader(DoorModuleLoader):
-  MODULE_NAME_PATTERN = 'door{}'
-  SUBDIR = TEMPLATES_BASE_PATH
-
-
-class DoorSolutionModuleLoader(DoorModuleLoader):
-  MODULE_NAME_PATTERN = 'tuerchen{}'
-  SUBDIR = BASE_PATH
-
-
-class DoorTesterModuleLoader(DoorModuleLoader):
-  MODULE_NAME_PATTERN = 'test{}'
-  SUBDIR = TEMPLATES_BASE_PATH
 
