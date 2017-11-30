751ede2bda3add85a047f77b74bdadb45b046e1b
Simon Feiden <simon.feiden@rwth-aachen.de>
2017-11-30 16:47:07 +0100

    Add api for tests

--- advent/test.py
+++ advent/test.py
@@ -3,6 +3,39 @@ import random
 from advent.doorio import DoorSuccessUpdate, DoorFile
 
 
+class Assert(object):
+  def __init__(self, function):
+    self.function = function
+    self.ncases = 0
+    self.npassed = 0
+    self.failed = []
+
+  def assert_true(self, expected, *args, **kwargs):
+    self.ncases += 1
+
+    actual = self.function(*args, **kwargs)
+
+    if expected == actual:
+      self.npassed += 1
+    else:
+      self.failed.append((expected, actual, args, kwargs))
+
+  def passed_all(self):
+    return self.npassed == self.ncases
+
+  @property
+  def nfailed(self):
+    return len(self.failed)
+
+  @classmethod
+  def all(clazz, function, cases):
+    t = clazz(function)
+    for expected, *args in cases:
+      t.assert_true(expected, *args)
+
+    return t
+
+
 class RandomCheer(object):
   def cheer(self):
     return random.choice(self.cheers)
@@ -25,19 +58,30 @@ def test(door):
   if tester is None:
     print('Huch, irgendwas ist schief gelaufen!')
     print('Ich kann das gerade nicht testen :(')
-    print('Starte mal `python advent.py update`.')
-    # return
+    print('Starte mal `python advent.py show {}`, vielleicht hilft das.'.format(door.day))
+    return
 
   if tested_mod is None:
     print('Huch, es gibt noch gar keine Lösung zum Türchen {}!'.format(door.day))
     print('Starte `python advent.py show {}` um dir das Türchen anzeigen zu lassen :)'.format(door.day))
     return
 
-  if tester.test(tested_mod):
+  asserted = tester.test(tested_mod, Assert)
+
+  if asserted.passed_all():
     DoorSuccessUpdate(door.day).update()
-    print(HappyCheer().cheer())
-    print('Tests bestanden, du hast das Türchen Nummer {} gelöst :)!'.format(door.day))
+    print(HappyCheer().cheer(), '{0} / {0} Tests bestanden.'.format(asserted.npassed))
+    print('Damit hast du das Türchen Nummer {} gelöst :)!'.format(door.day))
   else:
     print(SadCheer().cheer())
-    print('Tests nicht bestanden, da musst du nochmal ran!')
+    print(asserted.nfailed, 'Tests nicht bestanden, da musst du nochmal ran!')
+    print('Hier siehst du, was schief gegangen ist:')
+    print()
+
+    for i, (expected, actual, args, kwargs) in enumerate(asserted.failed):
+      print('Test', i)
+      print('  Eingabe:', *args)
+      print('  Erwartete Ausgabe:', expected)
+      print('  Deine Antwort:', actual)
+      print()
 
