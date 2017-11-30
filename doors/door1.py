7c89b88124fbc0ae38e26f05ed644d7c338770b6
Simon Feiden <simon.feiden@rwth-aachen.de>
2017-11-30 16:48:53 +0100

    Door 1

--- doors/door1.py
+++ doors/door1.py
@@ -1,5 +1,15 @@
 """
-special door 1
+Geschenkeliste
 
-description door 1
+Der Weihnachtsmann hat dieses Jahr viele Wunschzettel bekommen.
+Aus den Zetteln hat er in eine lange Liste geschrieben, wie viele
+Geschenke auf jedem Wunschzettel stehen.
+Wie viele Geschenke wurden insgesamt gewünscht?
 """
+
+def anzahl_geschenke(liste):
+  anzahl = 0
+
+  # Berechne hier die Anzahl der Geschenke
+
+  return anzahl
--- doors/test1.py
+++ doors/test1.py
@@ -1,2 +1,9 @@
-def test(tested_mod):
-  return True
+def test(tested_mod, Assert):
+  test_cases = [
+    (0, []),
+    (4, [4]),
+    (6, [1,2,3]),
+    (45, [1,2,3,4,5,6,7,8,9])
+  ]
+
+  return Assert.all(tested_mod.anzahl_geschenke, test_cases)
